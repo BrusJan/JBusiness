61130ea54467419abd54d6dbebe14d13a9e1dfd0
BrusJan <BrusJan@users.noreply.github.com>
2019-09-27 00:08:56 +0200

    Logo, login layout, navbar tweaks

--- client/src/app/layout/layout.component.ts
+++ client/src/app/layout/layout.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { navItems } from '../../app/_nav';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html'
 })
 export class LayoutComponent implements OnInit {
+  public navItems = navItems;
 
   constructor() { }
 
